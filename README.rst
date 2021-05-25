4c67a7231007f738cf48cfc6e62a9fa195b81ffa
hsorby <h.sorby@auckland.ac.nz>
2021-05-25 12:49:42 +1200

    Initial commit of MBF XML 2 Ex converter step.

---
+++ README.rst
@@ -0,0 +1,5 @@
+mbfxml2exconverter
+==================
+
+The mbfxml2exconverter step is a plugin for the MAP Client application.
+
---
+++ mapclientplugins/mbfxml2exconverterstep/__init__.py
@@ -0,0 +1,16 @@
+
+"""
+MAP Client Plugin
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Hugh Sorby'
+__stepname__ = 'mbfxml2exconverter'
+__location__ = 'https://github.com/hsorby/mbfxml2exconverterstep.git'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.mbfxml2exconverterstep import step
+
+# Import the resource file when the module is loaded,
+# this enables the framework to use the step icon.
+from . import resources_rc
