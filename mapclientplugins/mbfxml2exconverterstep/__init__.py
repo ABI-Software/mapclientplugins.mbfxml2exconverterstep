b9a959e5545fc09305d0d02382c2ac3ccc02438a
hsorby <h.sorby@auckland.ac.nz>
2021-11-02 00:47:42 +1300

    Set location to source code in step metadata to ABI-Software.

--- mapclientplugins/mbfxml2exconverterstep/__init__.py
+++ mapclientplugins/mbfxml2exconverterstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'mbfxml2exconverter'
-__location__ = 'https://github.com/hsorby/mbfxml2exconverterstep.git'
+__location__ = 'https://github.com/ABI-Software/mbfxml2exconverterstep.git'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.mbfxml2exconverterstep import step
